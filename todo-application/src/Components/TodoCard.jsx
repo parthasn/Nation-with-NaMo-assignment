a36a4ae843d6eb8de2e39dcc0be6fea55616d015
parthasn <parthasnayak0610@gmail.com>
2020-12-13 19:00:06 +0530

    Added styling

--- todo-application/src/Components/TodoCard.jsx
+++ todo-application/src/Components/TodoCard.jsx
@@ -7,56 +7,101 @@ import Grid from '@material-ui/core/Grid';
 import { toggleTask, filter } from '../Redux/App/actions';
 import ReactHashtag from 'react-hashtag';
 import Checkbox from '@material-ui/core/Checkbox';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     root: {
         flexGrow: 1
     },
-    paper: {
+    paper_completed: {
         padding: '10px',
         margin: '10px',
-        textAlign: 'center',
-        color: 'black'
+        backgroundColor: ' #000d33',
+        color: 'white',
+        textAlign: 'left',
+        position: "relative"
+    },
+    paper_pending: {
+        padding: '10px',
+        margin: '10px',
+        backgroundColor: '#ccd9ff',
+        color: '#00061a',
+        textAlign: 'left',
+        position: "relative"
+    },
+    checkbox: {
+        position: "absolute",
+        right: "40px",
+        bottom: 0
+    },
+    deleteCompleted: {
+        height: "25px",
+        position: "absolute",
+        right: "5px",
+        bottom: "7px"
+    },
+    deletePending: {
+        height: "25px",
+        position: "absolute",
+        right: "2px",
+        bottom: "0px"
     }
+   
 }));
 
 function TodoCard({ data }) {
     const classes = useStyles();
     const dispatch = useDispatch();
-    const [checked, setChecked] = React.useState(data.status);
+    const [ checked, setChecked ] = React.useState(data.status);
     const { title, status, id } = data;
 
     const handleToggle = () => {
         dispatch(toggleTask(id));
-        setChecked(true)
+        setChecked(true);
         console.log('toggle', id);
     };
 
     const handleHashtag = (value) => {
-        dispatch(filter(value))
+        dispatch(filter(value));
         console.log('hashtag', value);
     };
     console.log('data', title, status);
     return (
         <div className="todoCard__container">
             <Grid container spacing={3}>
-                <Grid item xs />
-                <Grid item xs={8}>
-                    <Paper
-                        style={{ backgroundColor: status ? 'green' : 'white' }}
-                        className={classes.paper}
-                    >
-                        <div>
-                            <Checkbox
-                                checked={checked}
-                                onChange={handleToggle}
-                                inputProps={{ 'aria-label': 'primary checkbox' }}
-                            />
-                            <ReactHashtag onHashtagClick={handleHashtag}>{title}</ReactHashtag>
-                        </div>
-                    </Paper>
+                <Grid item xs={12}>
+                    {status ? (
+                        <Paper className={classes.paper_completed}>
+                            <div>
+                                
+                                <ReactHashtag className = {classes.hashtag} onHashtagClick={handleHashtag}>
+                                    {title}
+                                </ReactHashtag>
+                                <DeleteIcon className = {classes.deleteCompleted}/>
+                                
+                                
+                            </div>
+                        </Paper>
+                    ) : (
+                        <Paper className={classes.paper_pending}>
+                            <div>
+                                <ReactHashtag  className = {classes.hashtag} onHashtagClick={handleHashtag}>
+                                    {title}
+                                </ReactHashtag>
+                                <Checkbox
+                                    className = {classes.checkbox}
+                                    checked={checked}
+                                    onChange={handleToggle}
+                                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                                    // style = {{float: "right"}}
+                                />
+                                <DeleteIcon className = {classes.deletePending}/>
+                                
+
+                            </div>
+                        </Paper>
+                    )}
                 </Grid>
-                <Grid item xs />
             </Grid>
         </div>
     );
